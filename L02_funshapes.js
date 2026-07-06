fb1d8471ee2063fecf82d23b8ca60c40eec8f906
Alexpoke2-sketch <balduvia@gmail.com>
2026-07-06 10:52:41 +0000

    week2

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -1,19 +1,55 @@
 /*
 Task 1: Add the setup() function
 */
+function setup() {
+
+    createCanvas(600,400);
+
+    background(220);
+
+}
 
 function draw() {
   // Challenge 1: An eye using ellipse() function
-
+  //  fill(250,0,0);
+  // ellipse(300,200,400,200);
+  //  fill(0,250,0);
+  // ellipse(300,200,100,200);
+  //  fill(0,0,250);
+  // ellipse(300,200,100,50);
   // Challenge 2: A face using circle() function
-
+  // circle(300,200,200);
+  // circle(200,100,80);
+  // circle(400,100,80);
+  // circle(275,150,25);
+  // circle(335,150,25);
+  // ellipse(300,250,80,40);
   // Challenge 3: A house using rect() function
-
+  // rect(100,50,400,300);
+  // circle(200,150,50);
+  // circle(400,150,50);
+  // rect(250,200,100,150);
+  // ellipse(300,250,20,30);
   // Challenge 4: A rocketship using triangle() function
-
+  // ellipse(300,200,150,300);
+  // circle(300,200,70);
+  // rect(225,300,150,50);
+  // triangle(300,10,210,150,390,150);
   // Challenge 5: A square face using quad() function
-
+  
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
+  // fill(255,150,0);
+  // triangle(100,200,250,230,10,10)
+  // fill(255,255,0);
+  // circle(300,200,300);
+  // fill(0,0,0);
+  // circle(200,150,25);
+  // circle(400,150,25);
+  // fill(255,0,0)
+  // circle(420,215,40);
+  // circle(180,215,40);
+  fill(255,255,255)
+  ellipse(150,)
 }
