f0c2f7ea265145d2949dba66f75c8dce7366a23f
Alexpoke2-sketch <balduvia@gmail.com>
2026-07-13 10:27:40 +0000

    Jul 13, 2026, 10:27 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -28,7 +28,7 @@ function draw() {
   //    rect(spacing +i*spacing,100,50,50);
   // }
   // Task 2: Colour Loop
-   fill(colour)
+   fill(colourVal)
   // Task 3: Row of Circles
 
   // Task 4: Grid of Circles
