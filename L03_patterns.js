aee42935a4f974ce3c7006994db95296459a8705
Alexpoke2-sketch <balduvia@gmail.com>
2026-07-13 10:17:29 +0000

    Jul 13, 2026, 10:17 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -21,7 +21,7 @@ function draw() {
   // }
 
   // Task 1: Colour Gradient
-  for(let i=10; i>0 ;i++ ) {
+  for(let i=10; i>0 ;i-- ) {
      fill(i * 50,255,255);
      rect(50+i*50,100,50,50);
   }
