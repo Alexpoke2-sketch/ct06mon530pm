332b93fe2bd530de345276177f22682fdb8e3f0d
Alexpoke2-sketch <balduvia@gmail.com>
2026-07-13 09:56:27 +0000

    Jul 13, 2026, 9:56 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -6,14 +6,18 @@ function setup() {
   
 function draw() {
   // Recap 1: Repeating Circles
-  fill(225);
-  circle(75,200,100);
-  fill(0,225,0);
-  circle(225,200,100);
-  fill(0,0,225);
-  circle(375,200,100);
-  fill(225,0,0);
-  circle(525,200,100);
+  // fill(225);
+  // circle(75,200,100);
+  // fill(0,225,0);
+  // circle(225,200,100);
+  // fill(0,0,225);
+  // circle(375,200,100);
+  // fill(225,0,0);
+  // circle(525,200,100);
+  for ( let i = 1; i < 5; i++) {
+    
+  }
+
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
