28d599de757c143ea10b77cb203b28d9fd52a318
Alexpoke2-sketch <balduvia@gmail.com>
2026-07-20 10:41:43 +0000

    Jul 20, 2026, 10:41 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -12,7 +12,7 @@ function draw() {
     //Task 1: Random Colour Preview
     fill(shapeColour)
     // circle(200,200,300)
-    triangle(random(0,400),random(0,400),random(0,400),random(0,400),random(),random());
+    triangle(random(0,400),random(0,400),random(0,400),random(0,400),random(0,400),random(0,400));
 }
 function mousePressed(){
     shapeColour = color(random(0,255),random(255),random(255));
