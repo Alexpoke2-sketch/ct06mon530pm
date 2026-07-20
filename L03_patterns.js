14fa9632e9e571a06c23b93d1eeae63e7160f01a
Alexpoke2-sketch <balduvia@gmail.com>
2026-07-20 09:54:39 +0000

    Jul 20, 2026, 9:54 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -40,7 +40,7 @@ function draw() {
   // Task 4: Grid of Circles
   let diameter = 30;
   let numCircle = 5;
-  let totalWidth = numCircles * circleDiameter;
+  let totalWidth = numcircle;
   // Calculate starting x for centering
    let startX = (width - totalWidth) / 2 + circleDiameter / 2;
 
