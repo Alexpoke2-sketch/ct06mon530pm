599c9d70cf065d51303da844d734ba2c9f929f1f
Alexpoke2-sketch <balduvia@gmail.com>
2026-07-27 10:33:30 +0000

    Jul 27, 2026, 10:33 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -6,31 +6,31 @@ function setup(){
     createCanvas(800,400);
 }
 function draw(){
-    background(220);
-    if(showCircle){
-        fill(random(255),random(255),random(255))
-        circle(width/2,height/2,100);
-    }
-    if(showTriangle){
-        fill(random(255),random(255),random(255))
-        triangle(width,height,width/2,height/2,100,100)
-    }
-    if(showSquare){
-        fill(random(255),random(255),random(255))
-        rect(width/2,height/2,100,100);
-    }
-}
-function keyPressed(){
-    if(key === 'c'){
-        showCircle = !showCircle;
-    }
-    if(key === 't'){
-        showTriangle = !showTriangle
-    }
-    if(key === 's'){
-        showSquare = !showSquare
-    }
-}
-function keyReleased(){
-    background(220);
+//     background(220);
+//     if(showCircle){
+//         fill(random(255),random(255),random(255))
+//         circle(width/2,height/2,100);
+//     }
+//     if(showTriangle){
+//         fill(random(255),random(255),random(255))
+//         triangle(width,height,width/2,height/2,100,100)
+//     }
+//     if(showSquare){
+//         fill(random(255),random(255),random(255))
+//         rect(width/2,height/2,100,100);
+//     }
+// }
+// function keyPressed(){
+//     if(key === 'c'){
+//         showCircle = !showCircle;
+//     }
+//     if(key === 't'){
+//         showTriangle = !showTriangle
+//     }
+//     if(key === 's'){
+//         showSquare = !showSquare
+//     }
+// }
+// function keyReleased(){
+//     background(220);
 }
