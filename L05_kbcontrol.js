993dbcd3ed5db796b08b387a03dc44d50a218b24
Alexpoke2-sketch <balduvia@gmail.com>
2026-07-27 09:58:51 +0000

    Jul 27, 2026, 9:58 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -4,12 +4,13 @@ function setup(){
     createCanvas(800,400);
 }
 function draw(){
-    background(220);
-    rect(width/2,height/2,rectsize,rectsize)
-}
-function keyPressed(){
-    rectsize = 100;
-}
-function keyReleased(){
-    rectsize = 50;
+//     background(220);
+//     rect(width/2,height/2,rectsize,rectsize)
+// }
+// function keyPressed(){
+//     rectsize = 100;
+// }
+// function keyReleased(){
+//     rectsize = 50;
+// }
 }
