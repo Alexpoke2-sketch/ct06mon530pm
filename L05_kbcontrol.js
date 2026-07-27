22c1c3404d3a67fb07fd90e64e2587810ea4efdd
Alexpoke2-sketch <balduvia@gmail.com>
2026-07-27 10:09:34 +0000

    Jul 27, 2026, 10:09 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,6 +1,7 @@
 let showCircle = false;
 let showTriangle = false;
-let show
+let showSquare = false;
+
 function setup(){
     createCanvas(800,400);
 }
@@ -9,6 +10,7 @@ function draw(){
     if(showCircle){
         circle(width/2,height/2,100);
     }
+    
 }
 function keyPressed(){
     if(key === 'c'){
