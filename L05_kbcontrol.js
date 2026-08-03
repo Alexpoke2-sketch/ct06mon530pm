2a1d9eddeffd31b7f45a169c68a294d056e99c17
Alexpoke2-sketch <balduvia@gmail.com>
2026-08-03 10:05:46 +0000

    Aug 3, 2026, 10:05 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -36,7 +36,7 @@ function keyReleased(){
     if(showCircle){
         background(220);
         fill(circlecolour,0,0);
-        circle(width/2,height/2,100)
+        circle(width/2,height/2,100);
     }
 
 }
