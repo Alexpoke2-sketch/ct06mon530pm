c4ea16d828140088ede1fec249d07d2a74c92b8a
Alexpoke2-sketch <balduvia@gmail.com>
2026-08-03 10:57:28 +0000

    modified:   L06_spritesoundfx.js

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,1 +1,38 @@
-// write your codes here
+let x = 50;
+let y = 50;
+let speed = 3;
+
+function setup(){
+    createCanvas(400,400);
+    square(x,y,50);
+}
+function draw(){
+    background(220);
+
+    // rect(50,50,300,300);
+    
+    if(keyIsDown(LEFT_ARROW)){
+        x -= speed;
+    }
+    if(keyIsDown(RIGHT_ARROW)){
+         x +=  speed;
+    }
+    if(keyIsDown(UP_ARROW)){
+        y -= speed;
+    }
+    if(keyIsDown(DOWN_ARROW)){
+        y += speed;
+    }
+
+    x = constrain(x,50,300);
+    y = constrain(y,50,300);
+    
+    if(keyIsDown(32)){
+        fill(50);
+    }else{
+        fill(200);
+    }
+    noStroke()
+    square(x,y,50);
+
+}
